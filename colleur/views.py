8acbe85fd7b9ce6a9c7a9f6672c10a54a1f744e6
stef <stephane.kirsch@gmail.com>
2018-10-05 13:18:17 +0800

    correction bug connexion colleur

--- colleur/views.py
+++ colleur/views.py
@@ -47,21 +47,24 @@ def is_profprincipal(user,classe=False):
 	else:
 		return bool(user.colleur.classeprofprincipal.all())
 
-def connec(request):
+def connec(request, id_matiere):
 	"""Renvoie la vue de la page de connexion des colleurs. Si le colleur est déjà connecté, redirige vers la page d'accueil des colleurs"""
+	matiere=get_object_or_404(Matiere,pk=id_matiere)
 	if is_colleur(request.user):
 		return redirect('action_colleur')
 	error = False
-	form = ColleurConnexionForm(request.POST or None)
+	form = ColleurConnexionForm(matiere,request.POST or None)
 	if form.is_valid():
-		user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
-		if user is not None and user.is_active and user.colleur and user.colleur.matieres.all():
+		username=form.cleaned_data['colleur'].user.username
+		user = authenticate(username=username,password=form.cleaned_data['password'])
+		if user is not None and user.is_active:
 			login(request,user)
-			request.session['matiere']=user.colleur.matieres.all()[0].pk
+			request.session['matiere']=Matiere.objects.filter(nom__iexact=matiere.nom,colleur=request.user.colleur)[0].pk
 			return redirect('action_colleur')
 		else:
 			error = True
-	return render(request,'colleur/home.html',{'form':form, 'error':error})
+	return render(request,'colleur/home.html',{'form':form, 'matiere':matiere,'error':error})
+
 
 @user_passes_test(is_colleur, login_url='accueil')
 def changemat(request,id_mat):
