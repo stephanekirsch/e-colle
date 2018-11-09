3255021bb17be634bc7ccc9a86bc8350767efd9b
stef <stephane.kirsch@gmail.com>
2018-11-09 20:03:57 +0800

    ajout d'une instruction "nouvelle_annee" pour vider une partie des données de la BDD entre 2 année scolaires

---
+++ accueil/management/commands/nouvelle_annee.py
@@ -0,0 +1,73 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection, transaction
+from ecolle.settings import BDD, MEDIA_ROOT
+import os
+ 
+class Command(BaseCommand):
+    help = """prépare la base de donnée pour une nouvelle année:
+    efface les messages, les notes, les colles, les semaines etc ....
+    conserve les classes/colleurs/eleves/matières/établissements
+    Il est conseillé de faire une sauvegarde de la base de donnée
+    avant de lancer cette commande"""
+
+    def handle(self, *args, **options):
+        if input("Êtes-vous sûr de vouloir réinitialiser la base de données? (o/n): ").lower() == "o":
+            self.stdout.write("lancement de la réinitialisation")
+            with connection.cursor() as cursor:
+                try:
+                    if BDD in ("postgresql","postgresql_pyscopg2"):
+                        # on vide les tables en question et on redémarre les ids à 1. 
+                        cursor.execute("TRUNCATE accueil_colle,\
+                            accueil_creneau,\
+                            accueil_destinataire,\
+                            accueil_jourferie,\
+                            accueil_message,\
+                            accueil_note,\
+                            accueil_noteects,\
+                            accueil_programme,\
+                            accueil_ramassage,\
+                            accueil_semaine\
+                            RESTART IDENTITY;")
+                        cursor.execute("VACUUM;")
+                    elif BDD == "mysql":
+                        cursor.execute("SET FOREIGN_KEY_CHECKS = 0;\
+                            TRUNCATE TABLE accueil_colle;\
+                            TRUNCATE TABLE accueil_creneau;\
+                            TRUNCATE TABLE accueil_destinataire;\
+                            TRUNCATE TABLE accueil_jourferie;\
+                            TRUNCATE TABLE accueil_message;\
+                            TRUNCATE TABLE accueil_note;\
+                            TRUNCATE TABLE accueil_noteects;\
+                            TRUNCATE TABLE accueil_programme;\
+                            TRUNCATE TABLE accueil_ramassage;\
+                            TRUNCATE TABLE accueil_semaine;\
+                            SET FOREIGN_KEY_CHECKS = 1;")
+                    else: # cas SQLite
+                        with transaction.atomic():
+                            cursor.execute("DELETE FROM accueil_colle;")
+                            cursor.execute("DELETE FROM accueil_creneau;")
+                            cursor.execute("DELETE FROM accueil_destinataire;")
+                            cursor.execute("DELETE FROM accueil_jourferie;")
+                            cursor.execute("DELETE FROM accueil_destinataire;")
+                            cursor.execute("DELETE FROM accueil_jourferie;")
+                            cursor.execute("DELETE FROM accueil_message;")
+                            cursor.execute("DELETE FROM accueil_note;")
+                            cursor.execute("DELETE FROM accueil_noteects;")
+                            cursor.execute("DELETE FROM accueil_programme;")
+                            cursor.execute("DELETE FROM accueil_ramassage;")
+                            cursor.execute("DELETE FROM accueil_semaine;")
+                        cursor.execute("VACUUM;")
+                except Exception as e:
+                    self.stdout.write("La réinitialisation a échoué: {}".format(e))
+                else:
+                    repertoire = os.path.join(MEDIA_ROOT,'programme')
+                    try:
+                        for fichier in os.listdir(repertoire):
+                            if fichier != ".gitignore":
+                                os.remove(os.path.join(repertoire,fichier))
+                    except Exception:
+                        self.stdout.write("Les fichiers programmes n'ont pas pu tous être effacés,\
+                            videz le répertoire media/programme à la main.")
+                    self.stdout.write("Réinitialisation complète")
+        else:
+            self.stdout.write("abandon")
