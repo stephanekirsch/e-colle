997094ef259059c7231df8881531f920342f865b
Stéphane Kirsch <stephane.kirsch@gmail.com>
2021-08-02 13:33:29 +0200

    adaptation de l'agenda aux groupes par semestre et aux options

--- accueil/models/colle.py
+++ accueil/models/colle.py
@@ -1,6 +1,9 @@
 from django.db import models, connection
+from django.shortcuts import get_object_or_404
+from .classe import Classe
 from .semaine import Semaine
 from .autre import Creneau, date_plus_jour, dictfetchall
+from .config import Config
 from django.db.models import Count
 from datetime import date, timedelta, datetime, time, timezone
 
@@ -102,6 +105,7 @@ class ColleManager(models.Manager):
 
     def agenda(self,colleur):
         semainemin = date.today()+timedelta(days=-28)
+        semestre2 = Config.objects.get_config().semestre2
         requete = "SELECT COUNT(n.id) nbnotes, co.id pk, g.nom nom_groupe, cl.nom nom_classe, {} jour, s.numero, cr.heure heure, cr.salle salle, m.id, m.nom nom_matiere, m.couleur couleur, m.lv lv, m.temps, u2.first_name prenom_eleve, u2.last_name nom_eleve, p.titre titre, p.detail detail, p.fichier fichier\
                    FROM accueil_colle co\
                    INNER JOIN accueil_creneau cr\
@@ -112,16 +116,17 @@ class ColleManager(models.Manager):
                    ON co.semaine_id=s.id\
                    INNER JOIN accueil_colleur c\
                    ON co.colleur_id = c.id\
+                   LEFT OUTER JOIN accueil_classe cl\
+                   ON cr.classe_id = cl.id\
                    LEFT OUTER JOIN accueil_groupe g\
                    ON co.groupe_id = g.id\
                    LEFT OUTER JOIN accueil_eleve e\
                    ON co.eleve_id = e.id\
                    LEFT OUTER JOIN accueil_eleve e2\
-                   ON e2.groupe_id = g.id\
+                   ON (((cl.semestres IS FALSE OR s.numero < %s) AND e2.groupe_id = g.id)\
+                   OR (cl.semestres IS TRUE AND s.numero >= %s AND e2.groupe2_id = g.id))\
                    LEFT OUTER JOIN accueil_user u2\
                    ON u2.eleve_id = e.id\
-                   LEFT OUTER JOIN accueil_classe cl\
-                   ON (co.classe_id = cl.id OR g.classe_id=cl.id OR e.classe_id=cl.id)\
                    LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier FROM accueil_programme_semaine ps\
                    INNER JOIN accueil_programme sub\
                    ON ps.programme_id = sub.id) p\
@@ -132,7 +137,7 @@ class ColleManager(models.Manager):
                    GROUP BY co.id, g.nom, g.id, cl.nom, s.id, cr.jour, cr.heure, cr.salle, m.id, m.nom, m.couleur, m.lv, m.temps, u2.first_name, u2.last_name, p.titre, p.detail, p.fichier\
                    ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
         with connection.cursor() as cursor:
-            cursor.execute(requete,(colleur.pk,semainemin))
+            cursor.execute(requete,(semestre2, semestre2, colleur.pk,semainemin))
             colles = dictfetchall(cursor)
         groupeseleve = self.filter(colleur=colleur,semaine__lundi__gte=semainemin,matiere__temps=20).select_related('matiere').prefetch_related('groupe__groupeeleve','groupe__groupeeleve__user')
         groupes = {}
@@ -143,59 +148,126 @@ class ColleManager(models.Manager):
         "detail":colle["detail"], "temps": colle["temps"], "pk": colle["pk"]} for colle in colles]
 
     def agendaEleve(self,eleve):
-        requete = "SELECT s.lundi lundi, s.numero, {} jour, cr.heure heure, cr.salle salle, m.nom nom_matiere, m.couleur couleur, u.first_name prenom, u.last_name nom, p.titre titre, p.detail detail, p.fichier fichier\
-                   FROM accueil_colle co\
-                   INNER JOIN accueil_creneau cr\
-                   ON co.creneau_id = cr.id\
-                   INNER JOIN accueil_matiere m\
-                   ON co.matiere_id = m.id\
-                   INNER JOIN accueil_semaine s\
-                   ON co.semaine_id=s.id\
-                   INNER JOIN accueil_colleur c\
-                   ON co.colleur_id=c.id\
-                   INNER JOIN accueil_user u\
-                   ON u.colleur_id=c.id\
-                   LEFT OUTER JOIN accueil_groupe g\
-                   ON co.groupe_id = g.id\
-                   INNER JOIN accueil_eleve e\
-                   ON e.groupe_id = g.id AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id) OR e.id=co.eleve_id\
-                   LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier FROM accueil_programme_semaine ps\
-                   INNER JOIN accueil_programme sub\
-                   ON ps.programme_id = sub.id) p\
-                   ON p.semaine_id = s.id AND p.matiere_id = m.id AND p.classe_id = %s\
-                   WHERE e.id=%s AND s.lundi >= %s\
-                   ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
-        with connection.cursor() as cursor:
-            cursor.execute(requete,(eleve.classe.pk,eleve.pk,date.today()+timedelta(days=-27)))
-            colles = dictfetchall(cursor)
+        if eleve.classe.semestres:
+            semestre2 = Config.objects.get_config().semestre2
+            requete = "SELECT s.lundi lundi, s.numero, {} jour, cr.heure heure, cr.salle salle, m.nom nom_matiere, m.couleur couleur, u.first_name prenom, u.last_name nom, p.titre titre, p.detail detail, p.fichier fichier\
+                       FROM accueil_colle co\
+                       INNER JOIN accueil_creneau cr\
+                       ON co.creneau_id = cr.id\
+                       INNER JOIN accueilclasse cl\
+                       ON cr.classe_id = cl.id\
+                       INNER JOIN accueil_matiere m\
+                       ON co.matiere_id = m.id\
+                       INNER JOIN accueil_semaine s\
+                       ON co.semaine_id=s.id\
+                       INNER JOIN accueil_colleur c\
+                       ON co.colleur_id=c.id\
+                       INNER JOIN accueil_user u\
+                       ON u.colleur_id=c.id\
+                       LEFT OUTER JOIN accueil_groupe g\
+                       ON co.groupe_id = g.id\
+                       INNER JOIN accueil_eleve e\
+                       ON (s.numero < %s AND e.groupe_id = g.id AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id))\
+                       OR (s.numero >= %s AND e.groupe2_id = g.id AND (m.id != cl.option1 AND m.id != cl.option2 OR e.option = m.id) AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id))\
+                       OR e.id=co.eleve_id\
+                       LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier FROM accueil_programme_semaine ps\
+                       INNER JOIN accueil_programme sub\
+                       ON ps.programme_id = sub.id) p\
+                       ON p.semaine_id = s.id AND p.matiere_id = m.id AND p.classe_id = %s\
+                       WHERE e.id=%s AND s.lundi >= %s\
+                       ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
+            with connection.cursor() as cursor:
+                cursor.execute(requete,(semestre2,semestre2,eleve.classe.pk,eleve.pk,date.today()+timedelta(days=-27)))
+                colles = dictfetchall(cursor)
+        else:
+            requete = "SELECT s.lundi lundi, s.numero, {} jour, cr.heure heure, cr.salle salle, m.nom nom_matiere, m.couleur couleur, u.first_name prenom, u.last_name nom, p.titre titre, p.detail detail, p.fichier fichier\
+                       FROM accueil_colle co\
+                       INNER JOIN accueil_creneau cr\
+                       ON co.creneau_id = cr.id\
+                       INNER JOIN accueil_classe cl\
+                       ON cr.classe_id = cl.id\
+                       INNER JOIN accueil_matiere m\
+                       ON co.matiere_id = m.id\
+                       INNER JOIN accueil_semaine s\
+                       ON co.semaine_id=s.id\
+                       INNER JOIN accueil_colleur c\
+                       ON co.colleur_id=c.id\
+                       INNER JOIN accueil_user u\
+                       ON u.colleur_id=c.id\
+                       LEFT OUTER JOIN accueil_groupe g\
+                       ON co.groupe_id = g.id\
+                       INNER JOIN accueil_eleve e\
+                       INNER JOIN accueil_eleve e\
+                       ON e.groupe_id = g.id AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id) OR e.id=co.eleve_id\
+                       LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier FROM accueil_programme_semaine ps\
+                       INNER JOIN accueil_programme sub\
+                       ON ps.programme_id = sub.id) p\
+                       ON p.semaine_id = s.id AND p.matiere_id = m.id AND p.classe_id = %s\
+                       WHERE e.id=%s AND s.lundi >= %s\
+                       ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
+            with connection.cursor() as cursor:
+                cursor.execute(requete,(eleve.classe.pk,eleve.pk,date.today()+timedelta(days=-27)))
+                colles = dictfetchall(cursor)
         return colles
 
     def agendaEleveApp(self,eleve):
-        requete = "SELECT s.numero, {} jour, cr.heure heure, cr.salle salle, m.nom nom_matiere, m.couleur couleur, u.first_name prenom, u.last_name nom, p.id id_programme\
-                   FROM accueil_colle co\
-                   INNER JOIN accueil_creneau cr\
-                   ON co.creneau_id = cr.id\
-                   INNER JOIN accueil_matiere m\
-                   ON co.matiere_id = m.id\
-                   INNER JOIN accueil_semaine s\
-                   ON co.semaine_id=s.id\
-                   INNER JOIN accueil_colleur c\
-                   ON co.colleur_id=c.id\
-                   INNER JOIN accueil_user u\
-                   ON u.colleur_id=c.id\
-                   LEFT OUTER JOIN accueil_groupe g\
-                   ON co.groupe_id = g.id\
-                   INNER JOIN accueil_eleve e\
-                   ON e.groupe_id = g.id AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id) OR e.id=co.eleve_id\
-                   LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier, sub.id FROM accueil_programme_semaine ps\
-                   INNER JOIN accueil_programme sub\
-                   ON ps.programme_id = sub.id) p\
-                   ON p.semaine_id = s.id AND p.matiere_id = m.id AND p.classe_id = %s\
-                   WHERE e.id=%s AND s.lundi >= %s\
-                   ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
-        with connection.cursor() as cursor:
-            cursor.execute(requete,(eleve.classe.pk,eleve.pk,date.today()+timedelta(days=-27)))
-            colles = dictfetchall(cursor)
+        if eleve.classe.semestres:
+            semestre2 = Config.objects.get_config().semestre2
+            requete = "SELECT s.numero, {} jour, cr.heure heure, cr.salle salle, m.nom nom_matiere, m.couleur couleur, u.first_name prenom, u.last_name nom, p.id id_programme\
+                       FROM accueil_colle co\
+                       INNER JOIN accueil_creneau cr\
+                       ON co.creneau_id = cr.id\
+                       INNER JOIN accueil_classe cl\
+                       ON cr.classe_id = cl.id\
+                       INNER JOIN accueil_matiere m\
+                       ON co.matiere_id = m.id\
+                       INNER JOIN accueil_semaine s\
+                       ON co.semaine_id=s.id\
+                       INNER JOIN accueil_colleur c\
+                       ON co.colleur_id=c.id\
+                       INNER JOIN accueil_user u\
+                       ON u.colleur_id=c.id\
+                       LEFT OUTER JOIN accueil_groupe g\
+                       ON co.groupe_id = g.id\
+                       INNER JOIN accueil_eleve e\
+                       ON (s.numero < %s AND e.groupe_id = g.id AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id))\
+                       OR (s.numero >= %s AND e.groupe2_id = g.id AND (m.id != cl.option1 AND m.id != cl.option2 OR e.option = m.id) AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id))\
+                       OR e.id=co.eleve_id\
+                       LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier, sub.id FROM accueil_programme_semaine ps\
+                       INNER JOIN accueil_programme sub\
+                       ON ps.programme_id = sub.id) p\
+                       ON p.semaine_id = s.id AND p.matiere_id = m.id AND p.classe_id = %s\
+                       WHERE e.id=%s AND s.lundi >= %s\
+                       ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
+            with connection.cursor() as cursor:
+                cursor.execute(requete,(semestre2, semestre2, eleve.classe.pk,eleve.pk,date.today()+timedelta(days=-27)))
+                colles = dictfetchall(cursor)
+        else:
+            requete = "SELECT s.numero, {} jour, cr.heure heure, cr.salle salle, m.nom nom_matiere, m.couleur couleur, u.first_name prenom, u.last_name nom, p.id id_programme\
+                       FROM accueil_colle co\
+                       INNER JOIN accueil_creneau cr\
+                       ON co.creneau_id = cr.id\
+                       INNER JOIN accueil_matiere m\
+                       ON co.matiere_id = m.id\
+                       INNER JOIN accueil_semaine s\
+                       ON co.semaine_id=s.id\
+                       INNER JOIN accueil_colleur c\
+                       ON co.colleur_id=c.id\
+                       INNER JOIN accueil_user u\
+                       ON u.colleur_id=c.id\
+                       LEFT OUTER JOIN accueil_groupe g\
+                       ON co.groupe_id = g.id\
+                       INNER JOIN accueil_eleve e\
+                       ON e.groupe_id = g.id AND (m.lv=0 OR m.lv=1 AND e.lv1_id=m.id OR m.lv=2 AND e.lv2_id=m.id) OR e.id=co.eleve_id\
+                       LEFT OUTER JOIN (SELECT ps.semaine_id, sub.matiere_id, sub.classe_id, sub.titre, sub.detail, sub.fichier, sub.id FROM accueil_programme_semaine ps\
+                       INNER JOIN accueil_programme sub\
+                       ON ps.programme_id = sub.id) p\
+                       ON p.semaine_id = s.id AND p.matiere_id = m.id AND p.classe_id = %s\
+                       WHERE e.id=%s AND s.lundi >= %s\
+                       ORDER BY s.lundi,cr.jour,cr.heure".format(date_plus_jour('s.lundi','cr.jour'))
+            with connection.cursor() as cursor:
+                cursor.execute(requete,(eleve.classe.pk,eleve.pk,date.today()+timedelta(days=-27)))
+                colles = dictfetchall(cursor)
         return [{'time': int(datetime.combine(agenda['jour'], time(*divmod(agenda['heure'],60))).replace(tzinfo=timezone.utc).timestamp()),
                                      'room':agenda['salle'],
                                      'week':agenda['numero'],
@@ -206,25 +278,48 @@ class ColleManager(models.Manager):
                                      } for agenda in colles]
 
     def compatEleve(self,id_classe):
-        requete = "SELECT COUNT(DISTINCT co.id) nbColles, COUNT(g.id), s.numero numero, cr.jour jour, cr.heure heure, u.first_name prenom, u.last_name nom\
-        FROM accueil_colle co\
-        LEFT OUTER JOIN accueil_groupe g\
-        ON co.groupe_id = g.id\
-        LEFT OUTER JOIN accueil_eleve e\
-        ON (e.id = co.eleve_id OR e.groupe_id = g.id)\
-        INNER JOIN accueil_semaine s\
-        ON co.semaine_id = s.id\
-        INNER JOIN accueil_user u\
-        ON u.eleve_id = e.id\
-        INNER JOIN accueil_creneau cr\
-        ON co.creneau_id = cr.id\
-        WHERE cr.classe_id=%s\
-        GROUP BY s.numero, cr.jour, cr.heure, u.first_name, u.last_name\
-        HAVING COUNT(DISTINCT co.id) > 1 AND COUNT(g.id) < COUNT(DISTINCT co.id)\
-        ORDER BY s.numero, cr.jour, cr.heure, u.first_name, u.last_name"
-        with connection.cursor() as cursor:
-            cursor.execute(requete,(id_classe,))
-            incompat = dictfetchall(cursor)
+        classe = get_object_or_404(Classe, pk=id_classe)
+        if classe.semestres:
+            semestre2 = Config.objects.get_config().semestre2
+            requete = "SELECT COUNT(DISTINCT co.id) nbColles, COUNT(g.id), s.numero numero, cr.jour jour, cr.heure heure, u.first_name prenom, u.last_name nom\
+            FROM accueil_colle co\
+            INNER JOIN accueil_semaine s\
+            ON co.semaine_id = s.id\
+            LEFT OUTER JOIN accueil_groupe g\
+            ON co.groupe_id = g.id\
+            LEFT OUTER JOIN accueil_eleve e\
+            ON (e.id = co.eleve_id OR s.numero < %s AND e.groupe_id = g.id OR s.numero >= %s AND e.groupe2_id = g.id)\
+            INNER JOIN accueil_user u\
+            ON u.eleve_id = e.id\
+            INNER JOIN accueil_creneau cr\
+            ON co.creneau_id = cr.id\
+            WHERE cr.classe_id=%s\
+            GROUP BY s.numero, cr.jour, cr.heure, u.first_name, u.last_name\
+            HAVING COUNT(DISTINCT co.id) > 1 AND COUNT(g.id) < COUNT(DISTINCT co.id)\
+            ORDER BY s.numero, cr.jour, cr.heure, u.first_name, u.last_name"
+            with connection.cursor() as cursor:
+                cursor.execute(requete,(semestre2, semestre2, id_classe))
+                incompat = dictfetchall(cursor)
+        else:
+            requete = "SELECT COUNT(DISTINCT co.id) nbColles, COUNT(g.id), s.numero numero, cr.jour jour, cr.heure heure, u.first_name prenom, u.last_name nom\
+            FROM accueil_colle co\
+            LEFT OUTER JOIN accueil_groupe g\
+            ON co.groupe_id = g.id\
+            LEFT OUTER JOIN accueil_eleve e\
+            ON (e.id = co.eleve_id OR e.groupe_id = g.id)\
+            INNER JOIN accueil_semaine s\
+            ON co.semaine_id = s.id\
+            INNER JOIN accueil_user u\
+            ON u.eleve_id = e.id\
+            INNER JOIN accueil_creneau cr\
+            ON co.creneau_id = cr.id\
+            WHERE cr.classe_id=%s\
+            GROUP BY s.numero, cr.jour, cr.heure, u.first_name, u.last_name\
+            HAVING COUNT(DISTINCT co.id) > 1 AND COUNT(g.id) < COUNT(DISTINCT co.id)\
+            ORDER BY s.numero, cr.jour, cr.heure, u.first_name, u.last_name"
+            with connection.cursor() as cursor:
+                cursor.execute(requete,(id_classe,))
+                incompat = dictfetchall(cursor)
         return incompat
 
 class Colle(models.Model):
