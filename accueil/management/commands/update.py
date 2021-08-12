c022cd557b5aa1c1e1c584e83e18315f88614ca8
Stéphane Kirsch <stephane.kirsch@gmail.com>
2021-08-13 01:46:12 +0200

    ajout de l'installation via pip d'éventuelles nouvelles bibliothèques

--- accueil/management/commands/update.py
+++ accueil/management/commands/update.py
@@ -88,8 +88,9 @@ class Command(BaseCommand):
                 poursuite = True
             if poursuite: # si la sauvegarde de la base de données a réussi ou si on s'en passe, on met à jour la migration
                 self.stdout.write("Mise à jour de la structure de la base de données")
-                chemin = os.path.join("..",cwd_new,"manage.py")
-                subprocess.run(["python3",chemin,"migrate"])
+                os.chdir(os.path.join("..",cwd_new))
+                subprocess.run(["pip3","install","requirements.txt"]) # on installe les éventuelles bilbiothèques manquantes
+                subprocess.run(["python3","manage.py","migrate"])
                 # on copie les fichiers media/backup:
                 self.stdout.write("copie des fichiers media/backup")
                 repertoires = os.path.listdir("media")
