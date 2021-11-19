5e457a304d7fe8416ae70bcd4eaaaad142bbe12f
stef <stephane.kirsch@gmail.com>
2021-11-19 20:48:50 +0100

    correction ordre d'affichage des programmes pour étudiants et colleurs

--- eleve/views.py
+++ eleve/views.py
@@ -72,7 +72,7 @@ def programme(request):
 	programmes = Programme.objects.filter(classe=eleve.classe).prefetch_related('semaine')
 	if matiere:
 		programmes=programmes.filter(matiere=matiere)
-	programmes = programmes.annotate(semainemax=Max('semaine')).select_related('matiere').order_by('-semainemax','matiere__nom')
+	programmes = programmes.annotate(semainemax=Max('semaine')).select_related('matiere').order_by('-semaine__numero','matiere__nom')
 	#programmes=programmes.values('matiere__couleur','matiere__nom','semaine__numero','titre','fichier','detail').order_by('-semaine__lundi','matiere__nom')
 	return render(request,'eleve/programme.html',{'form':form,'matiere':matiere,'programmes':programmes,'media_url':MEDIA_URL,'jpeg':IMAGEMAGICK})
 
