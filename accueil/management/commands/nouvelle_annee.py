e35f2141eaa8b2affe37f685a502f1b44c48af06
stef <stephane.kirsch@gmail.com>
2024-09-03 11:17:36 +0200

    correction bug nouvelle annee

--- accueil/management/commands/nouvelle_annee.py
+++ accueil/management/commands/nouvelle_annee.py
@@ -35,6 +35,7 @@ class Command(BaseCommand):
                         accueil_ramassage,\
                         accueil_semaine,\
                         accueil_planche,\
+                        accueil_planche_classes,\
                         django_session\
                         RESTART IDENTITY;")
                     cursor.execute("VACUUM;")
@@ -58,6 +59,7 @@ class Command(BaseCommand):
                         TRUNCATE TABLE accueil_ramassage;\
                         TRUNCATE TABLE accueil_semaine;\
                         TRUNCATE TABLE accueil_planche;\
+                        TRUNCATE TABLE accueil_planche_classes;\
                         TRUNCATE TABLE django_session;\
                         SET FOREIGN_KEY_CHECKS = 1;")
                 else: # cas SQLite
@@ -80,6 +82,7 @@ class Command(BaseCommand):
                         cursor.execute("DELETE FROM accueil_ramassage;")
                         cursor.execute("DELETE FROM accueil_semaine;")
                         cursor.execute("DELETE FROM accueil_planche;")
+                        cursor.execute("DELETE FROM accueil_planche_classes;")
                         cursor.execute("DELETE FROM django_session;")
                     cursor.execute("VACUUM;")
             except Exception as e:
